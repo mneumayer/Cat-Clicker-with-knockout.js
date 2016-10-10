bb8e55316463d4869571c2c0624dc2b818c64bbc
mneumayer <matthew.neumayer@gmail.com>
2016-10-10 15:30:16 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,14 +1,42 @@
-var Cat = function(){
-  this.clickCount = ko.observable(0);
-  this.name = ko.observable('Tabby');
+var initialCats = [{
+    clickCount: 0,
+    name: 'Tabby',
+    imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/bigtallguy/434164568',
+    nickNames: ['Tabtab', 'T-Bone', 'Mr. T', 'Tabitha Tab Tabby Catty Cat']
+}, {
+    clickCount: 0,
+    name: 'Tiger',
+    imgSrc: 'img/4154543904_6e2428c421_z.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/xshamx/4154543904',
+    nickNames: ['Tigger']
+}, {
+    clickCount: 0,
+    name: 'Scaredy',
+    imgSrc: 'img/22252709_010df3379e_z.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/kpjas/22252709',
+    nickNames: ['Casper']
+}, {
+    clickCount: 0,
+    name: 'Shadow',
+    imgSrc: 'img/1413379559_412a540d29_z.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/malfet/1413379559',
+    nickNames: ['Shooby']
+}, {
+    clickCount: 0,
+    name: 'Sleepy',
+    imgSrc: 'img/9648464288_2516b35537_z.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/onesharp/9648464288',
+    nickNames: ['Zzzzz']
+}];
+
+var Cat = function(data){
+  this.clickCount = ko.observable(data.clickCount);
+  this.name = ko.observable(data.name);
   this.catLevel = ko.observable("New Born");
-  this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-  this.imgAttribution = ko.observable('https:///www.flickr.com/photos/big cats');
-  this.nickName = ko.observableArray([
-    {name: 'LittleGuy'},
-    {name: 'FuzzBall'},
-    {name: 'Kitty'}
-  ]);
+  this.imgSrc = ko.observable(data.imgSrc);
+  this.imgAttribution = ko.observable(data.imgAttribution);
+  this.nickNames = ko.observableArray(data.nickNames);
 
 
 
@@ -45,11 +73,24 @@ var Cat = function(){
 
 var Viewmodel = function() {
 
-      this.currentCat = ko.observable(new Cat() );
+  var self = this;
+
+  this.catList = ko.observableArray([]);
+
+  initialCats.forEach(function(catItem){
+    self.catList.push ( new Cat(catItem));
+  });
+      this.currentCat = ko.observable(this.catList()[0]);
 
       this.incrementCounter = function() {
-        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+      self.currentCat().clickCount(self.currentCat().clickCount() + 1);
   };
+
+this.setCat = function(clickedCat){
+
+  self.currentCat(clickedCat)
+};
+
 
 };
 
